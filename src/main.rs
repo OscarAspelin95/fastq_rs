ad4497bf639f7d0f084baab7c6aad5304bd35e0f
Oscar Aspelin <oscar.aspelin@1928diagnostics.com>
2025-07-24 13:35:56 +0200

    Rework entire project and add mean length and error calculations as well as json output

--- src/main.rs
+++ src/main.rs
@@ -7,26 +7,22 @@ mod fastq;
 use fastq::parser::fastq_parser;
 
 #[derive(Parser, Debug)]
-#[command(version = "0.0.1", long_about = "Module for ")]
+#[command(version = "0.0.1", long_about = "Blazing fast fastq stats.")]
 struct Args {
-    #[arg(long)]
+    #[arg(short, long)]
     fastq: PathBuf,
 
-    #[arg(long)]
-    min_len: usize,
-
-    #[arg(long)]
-    min_phred: u8,
+    #[arg(short, long)]
+    outfile: PathBuf,
 }
 
 fn main() {
     SimpleLogger::new().init().unwrap();
 
     info!("Running main.rs");
-
     let args: Args = Args::parse();
 
     // Extract stats for each read.
     info!("Parsing fastq...");
-    fastq_parser(&args.fastq, args.min_len, args.min_phred);
+    fastq_parser(&args.fastq, &args.outfile);
 }
