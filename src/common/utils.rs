6a74d8eef326682ce274fa46670e537dcda4fd62
Oscar Aspelin <oscar.aspelin@enad.io>
2025-12-15 12:32:55 +0100

    Add rstest with tests for utils

--- src/common/utils.rs
+++ src/common/utils.rs
@@ -1,5 +1,8 @@
 use lazy_static::lazy_static;
+use rstest::rstest;
 use std::collections::HashMap;
+
+const PHRED_OFFSET: usize = 33;
 
 lazy_static! {
     pub static ref PHRED_TO_ERROR: [f64; 126] = {
@@ -7,7 +10,7 @@ lazy_static! {
 
         for i in 0..126 {
             if i >= 33 {
-                error_lookup[i] = 10_f64.powf(-((i - 33) as f64) / 10.0);
+                error_lookup[i] = 10_f64.powf(-((i - PHRED_OFFSET) as f64) / 10.0);
             };
         }
 
@@ -22,11 +25,13 @@ pub fn error_to_phred(error: f64) -> u8 {
 
 #[inline]
 pub fn mean_error_and_phred(qual: &[u8]) -> (f64, u8) {
+    if qual.is_empty() {
+        return (0.0, 0);
+    }
+
     let error_sum: f64 = qual
         .iter()
-        .map(|phred| {
-            PHRED_TO_ERROR[*phred as usize]
-        })
+        .map(|phred| PHRED_TO_ERROR[*phred as usize])
         .sum::<f64>();
 
     let error_mean = error_sum / qual.len() as f64;
@@ -35,11 +40,19 @@ pub fn mean_error_and_phred(qual: &[u8]) -> (f64, u8) {
 
 #[inline]
 pub fn mean_len(lengths: &[usize]) -> usize {
+    if lengths.len() == 0 {
+        return 0;
+    }
+
     lengths.iter().sum::<usize>() / lengths.len()
 }
 
 #[inline]
 pub fn nucleotide_counts(seq: &[u8]) -> (HashMap<&u8, usize>, usize, usize) {
+    if seq.len() == 0 {
+        return (HashMap::new(), 0, 0);
+    }
+
     let mut canonical: HashMap<&u8, usize> = HashMap::with_capacity(4);
 
     // Counts of non-canonical nucleotides.
@@ -72,6 +85,8 @@ pub fn nucleotide_counts(seq: &[u8]) -> (HashMap<&u8, usize>, usize, usize) {
 }
 
 #[inline]
+/// Using a pre-allocated vector with a .extend()
+/// might improve performance slightly, not sure.
 pub fn reverse_complement(seq: &[u8]) -> Vec<u8> {
     let reverse_complement: Vec<u8> = seq
         .iter()
@@ -100,4 +115,58 @@ pub fn reverse_complement(seq: &[u8]) -> Vec<u8> {
         .collect();
 
     reverse_complement
+}
+
+#[rstest]
+#[case(30 + PHRED_OFFSET, 0.001)]
+#[case(40 + PHRED_OFFSET, 0.0001)]
+#[case(50 + PHRED_OFFSET, 0.00001)]
+fn test_phred_to_error(#[case] phred: usize, #[case] expected: f64) {
+    assert_eq!(PHRED_TO_ERROR[phred], expected);
+}
+
+#[rstest]
+#[case(vec![], 0)]
+#[case(vec![10, 20, 30], 20)]
+fn test_mean_len(#[case] lengths: Vec<usize>, #[case] expected: usize) {
+    assert_eq!(mean_len(&lengths), expected);
+}
+
+#[rstest]
+#[case(b"A", vec![b'T'])]
+#[case(b"ATA", vec![b'T', b'A', b'T'])]
+fn test_reverse_complement(#[case] seq: &[u8], #[case] expected: Vec<u8>) {
+    assert_eq!(reverse_complement(seq), expected);
+}
+
+#[rstest]
+#[case(b"N", vec![b'N'])]
+#[case(b"TNT", vec![b'A', b'N', b'A'])]
+fn test_reverse_complement_ambig(#[case] seq: &[u8], #[case] expected: Vec<u8>) {
+    assert_eq!(reverse_complement(seq), expected);
+}
+
+#[rstest]
+#[case(b"", 0, 0, vec![])]
+#[case(b"aaAA", 2, 0, vec![(b'A', 2)])]
+#[case(b"aaAAttTTccCCggGGNN", 8, 2, vec![(b'A', 2), (b'T', 2), (b'C', 2), (b'G', 2)])]
+fn test_nucleotide_counts(
+    #[case] seq: &[u8],
+    #[case] expected_softmasked: usize,
+    #[case] expected_hardmasked: usize,
+    #[case] expected_counts: Vec<(u8, usize)>,
+) {
+    let (counts, softmasked, hardmasked) = nucleotide_counts(seq);
+
+    assert_eq!(softmasked, expected_softmasked);
+    assert_eq!(hardmasked, expected_hardmasked);
+
+    for (nt, expected_count) in expected_counts.into_iter() {
+        assert_eq!(
+            counts
+                .get(&nt)
+                .expect("Nucleotide {nt} has incorrect count {expected_count}"),
+            &expected_count
+        );
+    }
 }
